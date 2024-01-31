d52ecf2b355aa20b43d93e9e9750945f82c1fdd8
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2024-01-31 11:02:52 -0500

    feat: changed diff's filler char to be `-`

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -5,4 +5,4 @@
 vim.opt.mouse = "" -- Disable mouse
 vim.g.shfmt_extra_args = "-i 3 -sr -kp"
 -- [Q: How do I get the diagonal lines in place of deleted lines in diff-mode?](https://github.com/sindrets/diffview.nvim?tab=readme-ov-file#tips-and-faq)
-vim.opt.fillchars:append { diff = "╱" }
+vim.opt.fillchars:append({ diff = "-" })
