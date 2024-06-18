0d4d52735228c69087485178cdb52f28d2ba04b6
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2024-06-18 16:35:11 -0400

    feat(nvim): added refactoring plugin

--- dot_config/nvim/lua/plugins/trial.lua
+++ dot_config/nvim/lua/plugins/trial.lua
@@ -261,6 +261,21 @@ local plugins = {
       vim.g.db_ui_use_nerd_fonts = 1
     end,
   },
+
+  {
+    -- [github](https://github.com/ThePrimeagen/refactoring.nvim)
+    -- The Refactoring library based off the Refactoring book by Martin Fowler.
+    {
+      "ThePrimeagen/refactoring.nvim",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+      },
+      config = function()
+        require("refactoring").setup()
+      end,
+    },
+  },
 }
 
 return plugins
