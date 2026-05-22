889fdbe7eaa27d5558f220869295a256dd693196
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2026-05-22 15:24:19 -0400

    feat(nvim): added fff The fastest and the most accurate file search toolkit for AI agents, Neovim, Rust, C, and NodeJS

--- dot_config/nvim/lua/plugins/trial.lua
+++ dot_config/nvim/lua/plugins/trial.lua
@@ -450,6 +450,55 @@ local plugins = {
       })
     end,
   },
+
+  {
+    -- [source](https://github.com/dmtrKovalenko/fff)
+    -- The fastest and the most accurate file search toolkit for AI agents, Neovim, Rust, C, and NodeJS
+    "dmtrKovalenko/fff.nvim",
+    build = function()
+      -- downloads a prebuilt binary or falls back to cargo build
+      require("fff.download").download_or_build_binary()
+    end,
+    -- for nixos:
+    -- build = "nix run .#release",
+    opts = {
+      debug = {
+        enabled = true,
+        show_scores = true,
+      },
+    },
+    lazy = false, -- the plugin lazy-initialises itself
+    keys = {
+      {
+        "<leader>tff",
+        function()
+          require("fff").find_files()
+        end,
+        desc = "FFFind files",
+      },
+      {
+        "<leader>tfg",
+        function()
+          require("fff").live_grep()
+        end,
+        desc = "LiFFFe grep",
+      },
+      {
+        "<leader>tfz",
+        function()
+          require("fff").live_grep({ grep = { modes = { "fuzzy", "plain" } } })
+        end,
+        desc = "Live fffuzy grep",
+      },
+      {
+        "<leader>tfc",
+        function()
+          require("fff").live_grep({ query = vim.fn.expand("<cword>") })
+        end,
+        desc = "Search current word",
+      },
+    },
+  },
 }
 
 return plugins
